a4dce203923011df38e4d45470f9da1143cad111
stijnborloo1979 <stijn.borloo@gmail.com>
2026-09-09 11:53:51 +0200

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,6 @@
 // VisiOffer - Service Worker
 // Timestamp: 2026-09-05T00:00:00Z
-var CACHE = "visioffer-v20";
+var CACHE = "visioffer-v21";
 var INDEX = "index.html";
 
 self.addEventListener("install", function(e) {
